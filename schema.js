4e188c412e52de9ff14f1458cda8436854cec934
Ken Duigraha Putra <kenduigraha@yahoo.com>
2016-11-28 16:00:44 +0700

    final

---
+++ schema.js
@@ -0,0 +1,184 @@
+import mongoose from 'mongoose'
+import {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLSchema,
+  GraphQLNonNull,
+  GraphQLList,
+  GraphQLString,
+  GraphQLInt
+} from 'graphql'
+
+let User = mongoose.model('User', {
+  id: mongoose.Schema.Types.ObjectId,
+  name: String,
+  age: Number
+})
+
+mongoose.connect('mongodb://localhost:27017/biodatadb', (err) => {
+  if(err){
+    console.log(err);
+  }else{
+    console.log(`mongo connected`);
+  }
+})
+
+let UserType = new GraphQLObjectType({
+  name: 'user',
+  fields: () => ({
+    id: {
+      type: GraphQLID,
+      description: 'ID User'
+    },
+    name: {
+      type: GraphQLString,
+      description: "Name of User"
+    },
+    age: {
+      type: GraphQLInt,
+      description: "Age of User"
+    }
+  })
+})
+
+let getAll = () => {
+  return new Promise((resolve, reject) => {
+    User.find((err, users) =>  {
+      if(err){
+        reject(err);
+      }else{
+        resolve(users)
+      }
+    })
+  })
+}
+
+let QueryType = new GraphQLObjectType({
+  name: 'Query',
+  fields: () => ({
+    users: {
+      type: new GraphQLList(UserType),
+      resolve: () => {
+        return getAll()
+      }
+    }
+  })
+})
+
+let MutationAdd = {
+  type: UserType,
+  description: "Add a user",
+  args: {
+    name: {
+      name: 'user\'s name',
+      type: new GraphQLNonNull(GraphQLString)
+    },
+    age: {
+      age: 'user\'s age',
+      type: GraphQLInt
+    }
+  },
+  resolve: (root, args) => {
+    let newUser = new User({
+      name: args.name,
+      age: args.age
+    })
+    newUser.id = newUser._id
+    return new Promise((resolve, reject) => {
+      newUser.save((err) => {
+        if(err){
+          reject(err)
+        }else{
+          resolve(newUser)
+        }
+      })
+    })
+  }
+}
+
+let MutationEdit = {
+  type: UserType,
+  description: "Edit a user",
+  args: {
+    id: {
+      name: 'Id name',
+      type: GraphQLID
+    },
+    name: {
+      name: 'Edit user\'s name',
+      type: new GraphQLNonNull(GraphQLString)
+    },
+    age: {
+      age: 'Edit user\'s age',
+      type: GraphQLInt
+    }
+  },
+  resolve: (root, args) => {
+    return new Promise((resolve, reject) => {
+      User.findOneAndUpdate({
+        id: args.id
+      }, {
+        name: args.name,
+        age: args.age
+      }, {
+          new: true
+      },
+      (err, edited_data) => {
+        if(err){
+          reject(err)
+        }else{
+          resolve(edited_data)
+        }
+      })
+    })
+  }
+}
+
+let MutationDelete = {
+  type: UserType,
+  description: "Delete a user",
+  args: {
+    id: {
+      name: 'Id name',
+      type: GraphQLID
+    }
+  },
+  resolve: (root, args) => {
+    return new Promise((resolve, reject) => {
+      User.findOneAndRemove({
+        id: args.id
+      },
+      (err, deleted_data) => {
+        if(err){
+          reject(err)
+        }else{
+          resolve(deleted_data)
+        }
+      })
+    })
+  }
+}
+
+let MutationType = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    add: MutationAdd,
+    edit: MutationEdit,
+    delete: MutationDelete
+  }
+})
+
+let schema = new GraphQLSchema({
+  query: QueryType,
+  mutation: MutationType
+})
+
+export default schema
+/*
+mutation{add(name:"ken", age:22) {
+  id name age
+}}
+query{users {
+  id name age
+}}
+*/
---
+++ test/api.biodata.test.js
@@ -0,0 +1,86 @@
+const chai = require('chai')
+const expect = chai.expect
+const chaiHTTP = require('chai-http')
+chai.use(chaiHTTP)
+const should = chai.should()
+
+const URL = 'http://localhost:3000/'
+
+describe('Add new biodata', () => {
+  it('it should add new biodata', (done) => {
+    chai.request(URL)
+      .post('api/biodata')
+      .send({
+        "name" : "testing",
+        "age" : 100
+      })
+      .end((err, res) => {
+        res.should.be.json
+        res.should.have.status(200)
+        // console.log(res.body);
+        res.body.data.add.name.should.equal("testing")
+        res.body.data.add.age.should.equal(100)
+        done()
+      })
+  })
+})
+
+describe('Update a biodata', () => {
+  let index = 0
+  it('it should update a specific memo', (done) => {
+    chai.request(URL)
+      .get('api/biodata')
+      .end((err, res) => {
+        chai.request(URL)
+          .put('api/biodata')
+          .send({
+            "name": "test edit from testing",
+            "age" : 500,
+            "id"  : res.body.data.users[index].id
+          })
+          .end((err, respond) => {
+            // console.log(respond.body);
+            respond.should.have.status(200);
+            respond.should.be.json;
+            respond.body.data.edit.name.should.equal("test edit from testing")
+            respond.body.data.edit.age.should.equal(500)
+            done()
+          })
+      })
+  })
+})
+
+describe('Get all biodata', () => {
+  it('it should return all list of biodata in databases', (done) => {
+    chai.request(URL)
+      .get('api/biodata')
+      .end((err, res) => {
+        expect(res.body).to.be.an('object');
+        res.should.be.json
+        res.should.have.status(200)
+        done()
+      })
+  })
+})
+
+describe('Delete all biodata', () => {
+  let index = 0
+  it('it should delete all biodata from database', (done) => {
+    chai.request(URL)
+      .get('api/biodata')
+      .end((err, res) => {
+        chai.request(URL)
+          .delete('api/biodata')
+          .send({
+            id: res.body.data.users[index].id
+          })
+          .end((err, del_res) => {
+            del_res.should.have.status(200);
+            del_res.should.be.json;
+            del_res.body.data.delete.name.should.equal(res.body.data.users[index].name)
+            del_res.body.data.delete.age.should.equal(res.body.data.users[index].age)
+            done()
+          })
+      })
+  })
+})
